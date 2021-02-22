440754d4224f981df654eff45f90fddcad2890f6
Axel Angelini <axel.angelini@gmail.com>
2021-02-21 21:41:55 -0300

    Adding testLocation working properly

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ class Field {
 	}
 
 	print() {
-		this.field[this.playerLocation[0]][this.playerLocation[1]] = '*';
+		this.field[this.playerLocation[0]][this.playerLocation[1]] = pathCharacter;
 		for (let i = 0; i < this.field.length; i++) {
 			console.log(this.field[i].join(''));
 		}
@@ -39,18 +39,45 @@ class Field {
 				this.askLocation();
 				break;
 		}
+		console.log(this.playerLocation);
+	}
+
+	testLocation() {
+		if (
+			this.playerLocation[1] < 0 ||
+			this.playerLocation[0] < 0 ||
+			this.playerLocation[0] >= this.field.length
+		) {
+			console.log('Ouch! You went off the map!!');
+			this.stopGame = true;
+		} else if (this.field[this.playerLocation[0]][this.playerLocation[1]] == hat) {
+			console.log('Great job! You found your hat!!!');
+			this.stopGame = true;
+		} else if (this.field[this.playerLocation[0]][this.playerLocation[1]] == hole) {
+			console.log('Sorry! You fell down a hole!!');
+			this.stopGame = true;
+		} else {
+			for (let i = 0; i < this.field.length; i++) {
+				if (this.playerLocation[1] >= this.field.length) {
+					console.log('Ouch! You went off the map!!');
+					this.stopGame = true;
+					break;
+				}
+			}
+		}
 	}
 
 	loopGame() {
 		while (!this.stopGame) {
 			this.print();
 			this.askLocation();
+			this.testLocation();
 		}
 	}
 }
 
 const myField = new Field([
-	['*', '░', 'O'],
+	['*', '░', '░'],
 	['░', 'O', '░'],
 	['░', '^', '░'],
 ]);
